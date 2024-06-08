221d2cd6b91214644c479a7501d97e3959946f9e
Ajay3224 <ajay.12215248@lpu.in>
2024-06-08 22:14:46 +0530

    Update premtiveDt.java

--- premtiveDt.java
+++ premtiveDt.java
@@ -11,8 +11,8 @@ public class premtiveDt {
 		byte b = 10;
 		System.out.println("b is: "+b);
 		
-		//byte b2 = 129;
-		//System.out.println("B is :"+b2);
+		byte b2 = (byte)129;
+		System.out.println("B is :"+b2);
 		
 		int i = b;
 		System.out.println("I is :"+i);
